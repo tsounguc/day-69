e303565c3071a5648b8c14c1fa49288a279edbcf
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-12-12 22:08:27 -0500

    create a WTForm in forms.py Called RegisterForm

--- forms.py
+++ forms.py
@@ -14,6 +14,12 @@ class CreatePostForm(FlaskForm):
 
 
 # TODO: Create a RegisterForm to register new users
+class RegisterForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = StringField("Password")
+    name = StringField("Name")
+    submit = SubmitField("Sign Me Up")
+
 
 
 # TODO: Create a LoginForm to login existing users
