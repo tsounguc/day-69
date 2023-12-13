d59f2c4c5bc33d4e10d1c7c4e839a5a5783a9d7f
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-12-13 09:51:32 -0500

    added LoginForm in forms.py

--- forms.py
+++ forms.py
@@ -15,14 +15,16 @@ class CreatePostForm(FlaskForm):
 
 # RegisterForm to register new users
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired(),])
-    password = PasswordField("Password", validators=[DataRequired(), ])
+    email = StringField("Email", validators=[DataRequired(), ])
+    password = PasswordField("Password", validators=[DataRequired()])
     name = StringField("Name", validators=[DataRequired()])
     submit = SubmitField("Sign Me Up")
 
 
-
-# TODO: Create a LoginForm to login existing users
-
+# LoginForm to login existing users
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Let Me In")
 
 # TODO: Create a CommentForm so users can leave comments below posts
