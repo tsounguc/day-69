a53ed4d979e82f4d3e6ece61543aec1e7e743f5e
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-12-13 20:57:11 -0500

    Create a table called Comment with tablename comments

--- main.py
+++ main.py
@@ -73,6 +73,12 @@ class BlogPost(db.Model):
     author_id = db.Column(db.Integer, ForeignKey("users.id"))
 
 
+class Comment(db.Model):
+    __tablename__ = "comments"
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text, nullable=False)
+
+
 with app.app_context():
     db.create_all()
 
