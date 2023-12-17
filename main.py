f02f3592a4b815ab0550864ac625715526809a75
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-12-16 21:14:48 -0500

    Use environment variables to store sensitive information

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import date
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
@@ -27,7 +28,7 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('secret_key')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -35,11 +36,12 @@ Bootstrap5(app)
 login_manager = LoginManager()
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)
 
 login_manager.init_app(app)
+
 
 # gravatar = Gravatar(app,
 #                     size=100,
