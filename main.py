bd365f244b944307f4e6251cdcb3d6d11781bf47
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2024-01-06 23:27:36 -0500

    fix bug with edit_post. Was trying to update author

--- main.py
+++ main.py
@@ -276,7 +276,7 @@ def edit_post(post_id):
         post.title = edit_form.title.data
         post.subtitle = edit_form.subtitle.data
         post.img_url = edit_form.img_url.data
-        post.author = current_user
+        # post.author = current_user
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
